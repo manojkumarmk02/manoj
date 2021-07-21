f7c57a97721b88a5257d9e8dc52ed5df6e2a2dbf
manojkumarmk02 <manojkumarmk0456@gmail.com>
2021-07-21 08:42:31 +0530

    Task Completed

---
+++ src/org/basic/DataTypes.java
@@ -0,0 +1,40 @@
+package org.basic;
+
+public class DataTypes {
+ public static void main(String[] args) {
+	byte age=22;
+	System.out.println("my age is ="+age);
+	byte seatNo=35;
+	System.out.println("my seatNo is ="+seatNo);
+	short mobilePin=3456;
+	System.out.println("my mobilePin is ="+mobilePin);
+	short atmPin=3896;
+	System.out.println("my atmPin is ="+atmPin);
+	int no=908754321;
+	System.out.println("my no is ="+no);
+	int num=908754321;
+	System.out.println("my num is ="+num);
+	long mobileNo=8766421345l;
+	System.out.println("my mobileNo is ="+mobileNo);
+	long phoneNo=9086543447l;
+	System.out.println("my phoneNo is ="+phoneNo);
+	float amo = 54.5433445677f;
+	System.out.println("my amo is ="+amo);
+	float value  = 87.58995474f;
+	System.out.println("my value  is ="+value );
+	double a = 46.542445677885;
+	System.out.println("my a is ="+a);
+	double b = 65.632344567745;
+	System.out.println("my b is ="+b);
+	char name= 'F';
+	System.out.println("my name is ="+name);
+	char pin= 'D';
+	System.out.println("my pin is ="+pin);
+	boolean result = false;
+	System.out.println("my result is ="+result);
+	boolean report = true;
+	System.out.println("my report is ="+report);
+	String email = "Manoj356@gmail.com";
+	System.out.println(email); 
+}
+}
